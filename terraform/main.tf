369cd315a78490e823e35aa628751039e46dcd66
Jacob Torrång <kopatheonlyone@hotmail.com>
2021-10-07 22:54:11 +0200

    Remove references to s3 and dynamodb

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,48 +1,8 @@
-resource "aws_s3_bucket" "terraform-state" {
-  bucket = "terraform-remote-state-storage-b0adf7e2-fcaa-4e8d-8825-358c01b89fb0"
-  acl    = "private"
-
-  versioning {
-    enabled = true
-  }
-
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        kms_master_key_id = aws_kms_key.terraform-bucket-key.arn
-        sse_algorithm     = "aws:kms"
-      }
-    }
-  }
-}
-
-resource "aws_s3_bucket_public_access_block" "block" {
-  bucket = aws_s3_bucket.terraform-state.id
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
-
-resource "aws_dynamodb_table" "terraform-state" {
-  name           = "terraform-state"
-  read_capacity  = 20
-  write_capacity = 20
-  hash_key       = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
-
-
 terraform {
   backend "s3" {
     encrypt        = "true"
-    bucket         = aws_s3_bucket.terraform_state.bucket
-    dynamodb_table = aws_dynamodb_table.terraform_state.name
+    bucket         = "terraform-state-storage-b0adf7e2-fcaa-4e8d-8825-358c01b89fb0"
+    dynamodb_table = "terraform-state"
     key            = "blackabbot/terraform.tfstate"
     region         = "eu-west-1"
     profile        = "ci"
