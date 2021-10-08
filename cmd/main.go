124fbc6aa6c8925d13c3b4e721c847eb16421f40
Jacob Torrång <kopatheonlyone@hotmail.com>
2021-10-08 12:42:28 +0200

    Fix json

--- cmd/main.go
+++ cmd/main.go
@@ -7,21 +7,46 @@ import (
 
 	"github.com/aws/aws-lambda-go/events"
 	runtime "github.com/aws/aws-lambda-go/lambda"
-	telegram "github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
-type MethodResponse struct {
-	Method string `json:"method"`
+const (
+	MethodSendMessage = "sendMessage"
+)
+
+type MessageChat struct {
+	Id int64 `json:"id"`
+}
+
+type MessageFrom struct {
+	LastName  string `json:"last_name"`
+	Id        int64  `json:"id"`
+	FirstName string `json:"first_name"`
+	Username  string `json:"username"`
+}
+
+type Message struct {
+	Date      int64       `json:"date"`
+	Chat      MessageChat `json:"chat"`
+	MessageId int64       `json:"message_id"`
+	From      MessageFrom `json:"from"`
+	Text      string      `json:"text"`
+}
+
+type Update struct {
+	UpdateId int64    `json:"update_id"`
+	Message  *Message `json:"message"`
 }
 
 type SendMessageMethodResponse struct {
-	MethodResponse
-	telegram.MessageConfig
-}
-
-type AnswerInlineQueryMethodResponse struct {
-	MethodResponse
-	telegram.InlineConfig
+	Method                string       `json:"method"`
+	ChatId                int64        `json:"chat_id"`
+	ChannelUsername       *string      `json:"channel_username"`
+	ReplyToMessageID      *int         `json:"reply_to_message_id"`
+	ReplyMarkup           *interface{} `json:"reply_markup"`
+	DisableNotification   *bool        `json:"disable_notification"`
+	Text                  *string      `json:"text"`
+	ParseMode             *string      `json:"parse_mode"`
+	DisableWebPagePreview *bool        `json:"disable_web_page_preview"`
 }
 
 func main() {
@@ -30,7 +55,7 @@ func main() {
 
 func handleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 
-	result := &telegram.Update{}
+	result := &Update{}
 	err := json.Unmarshal([]byte(request.Body), result)
 	if err != nil {
 		return events.APIGatewayProxyResponse{Body: fmt.Sprintf("%v", err), StatusCode: 400}, nil
@@ -43,15 +68,13 @@ func handleRequest(ctx context.Context, request events.APIGatewayProxyRequest) (
 	return events.APIGatewayProxyResponse{StatusCode: 200}, nil
 }
 
-func handleMessage(update *telegram.Update) (events.APIGatewayProxyResponse, error) {
+func handleMessage(update *Update) (events.APIGatewayProxyResponse, error) {
+	text := "Hello World!"
+
 	response := SendMessageMethodResponse{
-		MethodResponse: MethodResponse{Method: "sendMessage"},
-		MessageConfig: telegram.MessageConfig{
-			BaseChat: telegram.BaseChat{
-				ChatID: update.Message.Chat.ID,
-			},
-			Text: "Hello World!",
-		},
+		Method: MethodSendMessage,
+		ChatId: update.Message.Chat.Id,
+		Text:   &text,
 	}
 
 	body, err := json.Marshal(response)
--- go.mod
+++ go.mod
@@ -2,9 +2,4 @@ module github.com/mewhhaha/blackabbot/v2
 
 go 1.17
 
-require (
-	github.com/aws/aws-lambda-go v1.27.0
-	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
-)
-
-require github.com/technoweenie/multipartstreamer v1.0.1 // indirect
+require github.com/aws/aws-lambda-go v1.27.0
