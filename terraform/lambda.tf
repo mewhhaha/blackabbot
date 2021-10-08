8da2b25fc4359ba75aeb84427fbd0426e89a6dd9
Jacob Torrång <kopatheonlyone@hotmail.com>
2021-10-08 22:59:23 +0200

    Remove sid

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -4,7 +4,7 @@ resource "aws_iam_role" "lambda_role" {
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
-      Sid : "AssumeRole"
+      Sid : ""
       Action : ["sts:AssumeRole"],
       Effect : "Allow",
       Principal : {
