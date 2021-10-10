7f1ce915ff1edce3f809d0645c4ea95d69354554
Jacob Torrång <kopatheonlyone@hotmail.com>
2021-10-11 00:54:04 +0200

    Add the data keyword

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -2,7 +2,8 @@ resource "aws_ecr_repository" "blackabbot" {
   name = "blackabbot"
 }
 
-data "aws_ecr_authorization_token" "token" {
+data "aws_ecr_authorization_token" "blackabbot" {
+  registry_id = aws_ecr_repository.blackabbot.registry_id
 }
 
 resource "null_resource" "image" {
@@ -12,7 +13,10 @@ resource "null_resource" "image" {
 
   provisioner "local-exec" {
     command = <<EOF
-          docker login --username AWS -p ${aws_ecr_authorization_token.token.password} ${aws_ecr_repository.blackabbot.repository_url}
+          docker login \
+            -u ${data.aws_ecr_authorization_token.blackabbot.user_name} \
+            -p ${data.aws_ecr_authorization_token.blackabbot.password} \
+            ${aws_ecr_repository.blackabbot.repository_url}
           docker tag ${var.webhook_image_id} ${aws_ecr_repository.blackabbot.repository_url}:latest
           docker push ${aws_ecr_repository.blackabbot.repository_url}:latest
     EOF
