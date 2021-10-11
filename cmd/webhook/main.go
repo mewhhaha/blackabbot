a53ee1555628a998b43e511056d65ae57995972a
Jacob Torrång <kopatheonlyone@hotmail.com>
2021-10-11 02:00:24 +0200

    Use dependency directly

--- cmd/webhook/main.go
+++ cmd/webhook/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"context"
+	"encoding/binary"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -20,8 +21,8 @@ import (
 	pollyT "github.com/aws/aws-sdk-go-v2/service/polly/types"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 	s3T "github.com/aws/aws-sdk-go-v2/service/s3/types"
-	"github.com/digital-dream-labs/opus-go/opus"
 	"github.com/google/uuid"
+	"gopkg.in/hraban/opus.v2"
 )
 
 var bucket = os.Getenv("AUDIO_BUCKET")
@@ -203,23 +204,34 @@ func saveToStorage(cfg aws.Config, audio io.ReadCloser) (*string, error) {
 
 	return &output.Location, nil
 }
-
 func convertToOpus(audio io.ReadCloser) (io.ReadCloser, error) {
-	pcm, err := ioutil.ReadAll(audio)
+
+	bs, err := ioutil.ReadAll(audio)
 	if err != nil {
 		return nil, err
 	}
 
-	stream := &opus.OggStream{
-		SampleRate: 48000,
-		Channels:   1,
-		Bitrate:    40000,
+	pcm := make([]int16, len(bs)/2)
+	for i := 0; i < len(bs)/2; i++ {
+		pcm[i] = int16(binary.LittleEndian.Uint16(bs[i*2:]))
 	}
 
-	data, err := stream.EncodeBytes(pcm)
+	const sampleRate = 48000
+	const channels = 1
+	const bufferSize = 1000
+
+	enc, err := opus.NewEncoder(sampleRate, channels, opus.AppVoIP)
 	if err != nil {
 		return nil, err
 	}
+
+	data := make([]byte, bufferSize)
+	n, err := enc.Encode(pcm, data)
+	if err != nil {
+		return nil, err
+	}
+
+	data = data[:n]
 
 	return io.NopCloser(bytes.NewReader(data)), nil
 }
--- go.mod
+++ go.mod
@@ -29,11 +29,7 @@ require (
 	github.com/aws/aws-sdk-go-v2/internal/ini v1.2.3 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sso v1.4.1 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sts v1.7.1 // indirect
-	github.com/digital-dream-labs/opus-go v0.0.0-20201230195736-934a8a9e0a1e
+	gopkg.in/hraban/opus.v2 v2.0.0-20210415224706-ab1467d63813
 )
 
-require (
-	github.com/grd/ogg v0.0.0-20130623210630-0dae53159b70 // indirect
-	github.com/jmespath/go-jmespath v0.4.0 // indirect
-	gopkg.in/hraban/opus.v2 v2.0.0-20210415224706-ab1467d63813 // indirect
-)
+require github.com/jmespath/go-jmespath v0.4.0 // indirect
